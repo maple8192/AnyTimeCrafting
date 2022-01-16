12e231b1ca34994ea8954c3c104773bf88d06349
Maple8192 <57043077+maple8192@users.noreply.github.com>
2022-01-16 23:01:32 +0900

    gradleプロジェクトに移行

--- src/main/kotlin/io/github/maple8192/anytimecrafting/listeners/CraftingOpenEvent.kt
+++ src/main/kotlin/io/github/maple8192/anytimecrafting/listeners/CraftingOpenEvent.kt
@@ -3,6 +3,7 @@ package io.github.maple8192.anytimecrafting.listeners
 import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.EquipmentSlot
 
@@ -13,8 +14,10 @@ class CraftingOpenEvent : Listener {
         val action = event.action
         val item = event.item
 
+        val isRightClick = action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK
+
         // 右クリックだったら
-        if (action.isRightClick) {
+        if (isRightClick) {
             // 多重検知防止
             if (event.hand != EquipmentSlot.HAND) return
 
